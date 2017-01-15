9719a7127f37af3b918a7177ad15fa898a78efd4
Vitaliy Bobrov <bobrov1989@gmail.com>
2017-01-15 19:25:26 +0100

    Added social share widget

--- helpers/post.js
+++ helpers/post.js
@@ -1,3 +1,5 @@
+const url = require('url');
+
 const postCell = (index, length) => {
   let desktop;
   let tablet;
@@ -74,7 +76,42 @@ const postIllustration = (tumb, alt) => `
       <img src="${tumb}.jpg" alt="${alt}" class="safe-picture__img">
     </picture>`;
 
+const postShare = (siteurl, path, id = 0) => {
+  let link = url.resolve(siteurl, path);
+
+  return `
+    <div class="mdl-card__menu post-share">
+      <button id="share-menu-${id}"
+              class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect post-share__button"
+              title="Share this post">
+        <svg class="mdl-svg post-share__icon">
+          <use xlink:href="#share"></use>
+        </svg>
+      </button>
+      <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect share-menu"
+          for="share-menu-${id}">
+        <li class="mdl-menu__item share-menu__item">
+          <a href="//twitter.com/home?status=${link}" class="share-menu__link" target="_blank" rel="nofollow">
+            <svg class="share-menu__icon">
+              <use xlink:href="#twitter"></use>
+            </svg>
+            Twitter
+          </a>
+        </li>
+        <li class="mdl-menu__item share-menu__item">
+          <a href="//www.facebook.com/sharer.php?u=${link}" class="share-menu__link" target="_blank" rel="nofollow">
+            <svg class="share-menu__icon">
+              <use xlink:href="#facebook"></use>
+            </svg>
+            Facebook
+          </a>
+        </li>
+      </ul>
+    </div>`;
+};
+
 module.exports = {
   postCell,
-  postIllustration
+  postIllustration,
+  postShare
 };
