8fc6fda438cf6f258c2ff81ba1df49b01a6b351b
Vitaliy Bobrov <bobrov1989@gmail.com>
2017-01-24 15:31:31 +0100

    Add aria-current for navigation

--- helpers/navigation.js
+++ helpers/navigation.js
@@ -1,5 +1,8 @@
 const activeLink = (path, link) => path === link ? 'mdl-navigation__link_active' : '';
 
+const currentLink = (path, link) => path === link ? 'aria-current="page"' : '';
+
 module.exports = {
-  activeLink
+  activeLink,
+  currentLink
 };
