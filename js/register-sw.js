ce7dc1483b099db762c8cebdefe12df04462b7e6
Vitalii Bobrov <bobrov1989@gmail.com>
2018-06-05 11:06:19 +0200

    Update SW

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,13 +1,15 @@
 ((window, document) => {
   'use strict';
 
+  const notification = document.querySelector('.mdl-js-snackbar');
+  const addBtn = document.querySelector('.js-add-to-home');
+
   /**
    * Registers SW.
    */
   function register() {
     navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
-        ga('send', 'event', 'Service Worker', 'install');
         // updatefound is fired if service-worker.js changes.
         registration.onupdatefound = () => {
           // updatefound is also fired the very first time the SW is installed,
@@ -16,7 +18,6 @@
           // i.e. whether there's an existing service worker.
           if (navigator.serviceWorker.controller) {
             const installingWorker = registration.installing;
-            const notification = document.querySelector('.mdl-js-snackbar');
             const data = {
               message: 'New content is available',
               actionHandler: () => window.location.reload(),
@@ -43,9 +44,71 @@
           }
         };
       }).catch(e => {
+        ga('send', 'event', 'Service Worker', 'error');
         console.error('Error during service worker registration:', e);
       });
   }
+
+  let installPromptEvent;
+  let tid;
+  const INSTALL_TIME = 5 * 1000 * 60;
+
+  /**
+   * Shows intall prompt to the user.
+   */
+  function showInstallPrompt() {
+    if (!installPromptEvent) return;
+
+    installPromptEvent.prompt();
+
+    // Wait for the user to respond to the prompt
+    installPromptEvent.userChoice.then(choice => {
+      if (choice.outcome === 'accepted') {
+        ga('send', 'event', 'Service Worker', 'prompt:accept');
+      } else {
+        ga('send', 'event', 'Service Worker', 'prompt:declined');
+      }
+
+      addBtn.disabled = true;
+      installPromptEvent = null;
+    });
+  }
+
+  /**
+   * Shows intall snackbar to the user.
+   */
+  function showInstallSnack() {
+    const data = {
+      message: `Seems that you enjoyed reading!
+      Would you like to add the blog to your home screen?`,
+      actionHandler: showInstallPrompt,
+      actionText: 'Add',
+      timeout: 10000
+    };
+
+    ga('send', 'event', 'Service Worker', 'prompt');
+    clearTimeout(tid);
+    notification.MaterialSnackbar.showSnackbar(data);
+  }
+
+  window.addEventListener('beforeinstallprompt', event => {
+    // Prevent Chrome <= 67 from automatically showing the prompt.
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    installPromptEvent = event;
+
+    addBtn.disabled = false;
+    tid = setTimeout(showInstallSnack, INSTALL_TIME);
+  });
+
+  window.addEventListener('appinstalled', () => {
+    ga('send', 'event', 'Service Worker', 'install');
+  });
+
+  addBtn.addEventListener('click', event => {
+    event.preventDefault();
+    showInstallPrompt();
+  }, false);
 
   if ('serviceWorker' in navigator &&
       window.location.protocol === 'https:') {
